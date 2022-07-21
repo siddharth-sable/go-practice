d180c40bc16cab30ed821c49d94af145ed8a6c9d
Siddharth <siddharth.sable4618@gmail.com>
2022-07-22 00:09:33 +0530

    Added go-postgres

---
+++ src/go-postgres/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"go-postgres/router"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := router.Router()
+	fmt.Println("Starting server on https://localhost:8080/")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
